1e61d3d23e5015186ad0c91042a86e20c3f83fba
Yevhen Khreptun <khreptun.e@gmail.com>
2023-08-02 21:03:19 -0400

    Added a static structure for the 03-travel-list project

--- 03-travel-list/src/App.js
+++ 03-travel-list/src/App.js
@@ -1,5 +1,41 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
 function App() {
-  return <></>;
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
 }
 
 export default App;
+
+function Logo() {
+  return <h1>🌴 Far away 💼</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return <div className="list">List</div>;
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>👜 You have X items on your list, and you already packed X%</em>
+    </footer>
+  );
+}
