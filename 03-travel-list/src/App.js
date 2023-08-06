a3b1af83cf9655bc36f922f2fd9a7ff1b7095c0b
Yevhen Khreptun <khreptun.e@gmail.com>
2023-08-06 14:22:29 -0400

    Created a statistic section

--- 03-travel-list/src/App.js
+++ 03-travel-list/src/App.js
@@ -28,7 +28,7 @@ function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -128,10 +128,26 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <footer className="stats">
+        <em>Start edding some items to your packing list 🚀!</em>
+      </footer>
+    );
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
   return (
     <footer className="stats">
-      <em>👜 You have X items on your list, and you already packed X%</em>
+      <em>
+        {percentage === 100
+          ? "You got everything! Ready to go! 🐬"
+          : `👜 You have ${numItems} items on your list, and you already packed
+          ${numPacked} ${percentage}%`}
+      </em>
     </footer>
   );
 }
