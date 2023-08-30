a601010e61ccef8f41fa22e33ca5d9c2380c7525
Yevhen Khreptun <khreptun.e@gmail.com>
2023-08-30 14:23:24 -0400

    Tested a wrong fetch data way for React

--- 05-usepopcorn/src/index.js
+++ 05-usepopcorn/src/index.js
@@ -1,21 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
+import "./index.css";
 // import App from "./App";
-import StarRating from "./StarRating";
+import App from "./App-v1";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {
       <>
-        {/* <App /> */}
-        <StarRating
-          maxRating={5}
-          messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
-          defaultRating={3}
-          onSetRating={() => {}}
-        />
+        <App />
       </>
     }
   </React.StrictMode>
