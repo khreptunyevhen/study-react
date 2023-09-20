598aa44cbb5a2449627981eedbe2d20909bdb670
Yevhen Khreptun <khreptun.e@gmail.com>
2023-09-19 20:42:55 -0400

    Static layout

--- 08-word-wide/src/App.jsx
+++ 08-word-wide/src/App.jsx
@@ -1,5 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+import NotFound from "./pages/NotFound";
+import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
+
 function App() {
-  return <>WordWide</>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
+        <Route path="app" element={<AppLayout />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
